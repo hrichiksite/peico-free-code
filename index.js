464fea4fd90ab4343a6f66785a1ec8486ff459a8
hrichiksite <hrichik.site@gmail.com>
2021-05-03 20:29:51 +0530

    IP

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.get('/', async (req, res) => {
               })
 
 app.get('/add', async (req, res) => {
-  var id = req.header['X-Real-IP'];
+  var id = req.header['CF-Connecting-IP'];
   var limit = new Limiter({ id: id, db: client });
   limit.get(function(err, limit){
     if (err) return next(err);
